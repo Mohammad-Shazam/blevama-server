18dc0d001267535bc2f5687af46b46334b147d88
Mohammad-Shazam <mosuleiman128@gmail.com>
2025-03-14 05:55:34 +0300

    Fixed port issue for Render deployment

--- server.js
+++ server.js
@@ -48,5 +48,5 @@ app.post('/send-email', (req, res) => {
 
 // Start server
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-});
+    console.log(`Server is running on port ${PORT}`);
+  });
